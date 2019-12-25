ca5cee742f6c2df93668b78f5eadda7e482007f9
inventivetalent <mail@inventivetalent.org>
2019-12-25 21:22:09 +0100

    material stuff

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,59 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="page-container" id="app">
+        <md-app>
+            <md-app-toolbar class="md-primary">
+                <span class="md-title">VRKnock</span>
+            </md-app-toolbar>
+            <md-app-content>
+                <router-view/>
+            </md-app-content>
+        </md-app>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    @import url("https://fonts.googleapis.com/css?family=Roboto:300,400,500");
+    @import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+    @import '~vue-material/dist/vue-material.min.css';
+    @import "~vue-material/dist/theme/default.css";
 
-#nav {
-  padding: 30px;
-}
+    #app {
+        font-family: 'Roboto', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    #nav {
+        padding: 30px;
+    }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
+
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
 </style>
+<style lang="scss">
+    @import "~vue-material/dist/theme/engine";
+
+    @include md-register-theme(
+                    "default",
+                    (
+                            primary: #790FBF,
+                            accent: #637300,
+                            theme: dark
+                    )
+    );
+
+    @import "~vue-material/dist/base/theme";
+    @import "~vue-material/dist/theme/all";
+</style>
+<script>
+    export default {}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter from "vue-router";
+import VueMaterial from 'vue-material'
+
 
 import App from './App.vue'
 import router from './router'
@@ -8,6 +10,7 @@ import router from './router'
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
+Vue.use(VueMaterial);
 
 new Vue({
   router,
