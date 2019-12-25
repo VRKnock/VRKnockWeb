9cbebae9a30808a5bc7fb27abef3cbd8f3905300
inventivetalent <mail@inventivetalent.org>
2019-12-25 19:12:36 +0100

    router

---
+++ src/components/Knocker.vue
@@ -0,0 +1,9 @@
+<template>
+<div class="knocker">
+    *Knock Knock*
+</div>
+</template>
+
+<script>
+
+</script>
---
+++ src/views/Knocker.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="knocker">
+        <h1>Knocker</h1>
+        <p>{{ $route.params.host }} {{ $route.params.code }}</p>
+    </div>
+</template>
