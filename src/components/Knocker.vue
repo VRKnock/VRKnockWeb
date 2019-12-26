46ad4c7537612a19b178793d7f682961c41390e6
inventivetalent <mail@inventivetalent.org>
2019-12-26 15:02:24 +0100

    getting started on knock page

--- src/components/Knocker.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<div class="knocker">
-    *Knock Knock*
-</div>
-</template>
-
-<script>
-
-</script>
--- src/views/Knocker.vue
+++ src/views/Knocker.vue
@@ -1,6 +1,44 @@
 <template>
     <div class="knocker">
-        <h1>Knocker</h1>
-        <p>{{ $route.params.host }} {{ $route.params.code }}</p>
+        <span class="connection-info" :v-model="connectionInfo"></span>
+
+        <md-button class="md-raised knock-button" :disabled="!canKnock">
+            <img :src="canKnock ? img('knocking_hand.svg') : img('knocking_hand_gray.svg')" class="knock-button-image">
+        </md-button>
     </div>
 </template>
+
+<style scoped>
+    .knock-button{
+        width: 150px;
+        height: 150px;
+        background-color: #D6D7D7;
+    }
+
+    .knock-button-image{
+        width: 128px;
+        height: 128px;
+    }
+</style>
+
+<script>
+
+    export  default {
+        name:"Knocker",
+        data:()=>({
+            connectionInfo:"",
+            canKnock: false
+        }),
+        methods:{
+            img(img){
+                return require("../assets/" + img);
+            },
+            testConnection(){
+                //TODO
+            },
+            triggerKnock(){
+                //TODO
+            }
+        }
+    }
+</script>
