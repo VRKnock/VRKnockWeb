3d6aa8403346c39d3180e2863ffcc9391762ab81
inventivetalent <mail@inventivetalent.org>
2019-12-27 15:51:51 +0100

    rewrite stuff to use websockets

--- src/views/Knocker.vue
+++ src/views/Knocker.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="knocker">
         <br/>
-        <span class="connection-info" :v-model="statusText"></span>
+        <span class="connection-info">{{ statusText }}</span>
         <br/>
-        <span class="game-activity" :v-model="gameActivity"></span>
+        <span class="game-activity">{{ gameActivity }}</span>
         <br/>
 
         <md-button class="md-raised knock-button" :disabled="!canKnock" @click="triggerKnock()">
@@ -42,8 +42,6 @@
 </style>
 
 <script>
-    import axios from "axios";
-
     export default {
         name: "Knocker",
         data: () => ({
@@ -53,7 +51,9 @@
             statusText: "",
             gameActivity: "",
             canKnock: false,
-            message: "Someone Wants Your Attention!"
+            message: "Someone Wants Your Attention!",
+            currentRequestResolve: null,
+            socket: null
         }),
         methods: {
             img(img) {
@@ -73,6 +73,7 @@
             onConnectionEstablished() {
                 this.canKnock = true;
 
+                this.statusText = "Connected!";
             },
             onConnectionLost(reason) {
                 if (!reason) {
@@ -95,29 +96,74 @@
             testConnection() {
                 this.$emit("snackbar", "Connecting...");
 
-                let body = {
-                    code: this.code,
-                    version: "web"
-                };
-                this.postJson(this.host, this.port, "status", body).then(json => {
-                    if (json) {
-                        let status = json["Status"];
+                // socket = io("ws://" + this.host + ":" + this.port,{
+                //     path:"/",
+                //     transports: ['websocket']
+                // });
+                this.socket = new WebSocket("ws://" + this.host + ":" + this.port);
 
-                        let msg = status["msg"];
-                        let game = status["game"];
+                // // https://stackoverflow.com/questions/10405070/socket-io-client-respond-to-all-events-with-one-handler/33960032#33960032
+                // var onevent = socket.onevent;
+                // socket.onevent = function (packet) {
+                //     var args = packet.data || [];
+                //     onevent.call (this, packet);    // original call
+                //     packet.data = ["*"].concat(args);
+                //     onevent.call(this, packet);      // additional call to catch-all
+                // };
+                // ///
 
-                        this.$emit("snackbar", msg);
-                        this.updateActivity(game);
+                this.socket.onopen = () => {
+                    window.console.log("Socket connected");
 
-                        if (status["status"] !== 0) {
-                            this.onConnectionLost();
+                    let body = {
+                        action: "status",
+                        code: this.code,
+                        version: "web"
+                    };
+                    this.sendRequest(body).then(json => {
+                        if (json) {
+                            let msg = json["msg"];
+                            let game = json["game"];
+
+                            this.$emit("snackbar", msg);
+                            this.updateActivity(game);
+
+                            if (json["status"] !== 0) {
+                                this.onConnectionLost();
+                            } else {
+                                this.onConnectionEstablished();
+                            }
                         } else {
-                            this.onConnectionEstablished();
+                            this.onConnectionLost();
                         }
-                    }else {
-                        this.onConnectionLost();
+                    })
+                };
+                this.socket.onerror = (err) => {
+                    window.console.warn(err);
+                    this.onConnectionLost("Socket Connection Error");
+                };
+                // socket.on("connect_timeout",()=>{
+                //     this.onConnectionLost("Socket Connection Timeout");
+                // });
+                // socket.on('disconnect', (reason) => {
+                //     if (reason === 'io server disconnect') {
+                //        this.onConnectionLost("Connection closed by server");
+                //     }
+                //     // else the socket will automatically try to reconnect
+                // });
+
+                this.socket.onmessage = (event) => {
+                    window.console.log(event.data);
+                    let parsed = JSON.parse(event.data);
+                    window.console.log(parsed);
+
+
+                    if (parsed && parsed.hasOwnProperty("evt") && parsed.evt === "status" && this.currentRequestResolve) {
+                        this.currentRequestResolve(parsed);
+                        this.currentRequestResolve = null;
                     }
-                })
+                }
+
             },
             triggerKnock() {
                 if (!this.canKnock) {
@@ -127,43 +173,43 @@
                 this.canKnock = false;
 
                 let body = {
+                    action: "triggerKnock",
                     code: this.code,
-                    message:this.message,
-                    version:"web"
+                    message: this.message,
+                    version: "web"
                 };
-                this.postJson(this.host, this.port, "status", body).then(json => {
+                this.sendRequest(body).then(json => {
+                    window.console.log(json);
                     if (json) {
-                        let status = json["Status"];
 
-                        let msg = status["msg"];
-                        let game = status["game"];
+                        let msg = json["msg"];
+                        let game = json["game"];
 
                         this.$emit("snackbar", msg);
                         this.updateActivity(game);
 
-                        if (status["status"] !== 0) {
+                        if (json["status"] !== 0) {
                             this.onConnectionLost();
                         } else {
                             setTimeout(() => {
                                 this.canKnock = true;
                             }, 2000);
                         }
-                    }else {
+                    } else {
                         this.onConnectionLost();
                     }
                 })
             },
-            postJson(host, port, path, body) {
-                return axios({
-                    method: "post",
-                    baseURL: "http://" + host + ":" + port + "/",
-                    url: path,
-                    data: body,
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'User-Agent': 'VRKnockWeb'
+            sendRequest(body) {
+                return new Promise(resolve => {
+                    if (!this.socket) {
+                        resolve(null);
+                        return;
                     }
-                })
+                    this.currentRequestResolve = resolve;
+
+                    this.socket.send(JSON.stringify(body));
+                });
             }
         },
         mounted() {
