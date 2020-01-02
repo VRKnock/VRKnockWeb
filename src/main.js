553a4b63eee690b0d22a2ba337844f690bb542d6
inventivetalent <mail@inventivetalent.org>
2020-01-02 14:05:08 +0100

    add smart-app-banner

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from "vue-router";
 import VueMaterial from 'vue-material'
 
+import SmartBanner from 'smart-app-banner'
+import 'smart-app-banner/dist/smart-app-banner.css'
 
 import App from './App.vue'
 import router from './router'
@@ -14,5 +16,23 @@ Vue.use(VueMaterial);
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  created(){
+    new SmartBanner({
+      daysHidden: 15,   // days to hide banner after close button is clicked (defaults to 15)
+      daysReminder: 90, // days to hide banner after "VIEW" button is clicked (defaults to 90)
+      title: 'VRKnock',
+      author: 'inventivetalent',
+      button: 'VIEW',
+      store: {
+        android: 'On Google Play',
+      },
+      price: {
+        ios: 'FREE',
+        android: 'FREE',
+        windows: 'FREE'
+      },
+      icon: 'https://vrknock.app/img/web_hi_res_512.png'
+    });
+  }
 }).$mount('#app')
